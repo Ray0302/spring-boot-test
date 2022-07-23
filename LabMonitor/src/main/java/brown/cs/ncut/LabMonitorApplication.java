31daf97d2dc84dd40f741a71eca2913795aa7e07
Ray0302 <ray910302@gmail.com>
2022-07-23 12:41:06 +0800

    idolt

---
+++ LabMonitor/src/main/java/brown/cs/ncut/LabMonitorApplication.java
@@ -0,0 +1,13 @@
+package brown.cs.ncut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabMonitorApplication.class, args);
+	}
+
+}
